3c9aafa909dda556329d287a692f62b34c5e945b
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-10 05:34:07 +0200

    code cleaned up

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import './Login.css';
 
 class Login extends Component {
 	// Data
@@ -11,9 +11,24 @@ class Login extends Component {
 	// Render
 	render() {
 		return (
-			<h1>Hello</h1>
+			<form>
+				<div class="form-group">
+					<label for="exampleInputEmail1">Email address</label>
+					<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+					<small id="emailHelp" class="form-text text-muted">Enter a valid Email address</small>
+				</div>
+				<div class="form-group">
+					<label for="exampleInputPassword1">Password</label>
+					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+				</div>
+				<div class="form-group form-check">
+					<input type="checkbox" class="form-check-input" id="exampleCheck1" />
+					<label class="form-check-label" for="exampleCheck1">Show password</label>
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
 		)
 	}
 }
 
-export default App;
+export default Login;
--- src/Signup.js
+++ src/Signup.js
@@ -11,7 +11,26 @@ class Signup extends Component {
 	// Render
 	render() {
 		return (
-			<h1>Hello</h1>
+			<form>
+				<div className="form-group">
+					<label for="exampleInputEmail1">Nickname</label>
+					<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter nickname..." />
+				</div>
+				<div className="form-group">
+					<label for="exampleInputEmail1">Email address</label>
+					<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+					<small id="emailHelp" className="form-text text-muted">Enter a valid Email address</small>
+				</div>
+				<div className="form-group">
+					<label for="exampleInputPassword1">Password</label>
+					<input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+				</div>
+				<div className="form-group form-check">
+					<input type="checkbox" className="form-check-input" id="exampleCheck1" />
+					<label className="form-check-label" for="exampleCheck1">Show password</label>
+				</div>
+				<button type="submit" className="btn btn-primary">Submit</button>
+			</form>
 		)
 	}
 }
