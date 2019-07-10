728b81368de06779f76814bc3a58352e7e3a6103
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-10 07:35:54 +0200

    function to change the values in the signup components created

--- src/Signup.js
+++ src/Signup.js
@@ -4,9 +4,26 @@ import './Signup.css';
 class Signup extends Component {
 	// Data
 	state = {
-
+		name:'',
+		email:'',
+		password:''
 	}
 	// Functions
+
+	//Functions which allow us to change the value inside the value attribute
+	// and set it equal to what the user type in the bar
+	//They will go in an onChange eventListener in the input tag
+	changeName = (e) => {
+		this.setState({name: e.target.value})
+	}
+
+	changeEmail = (e) => {
+		this.setState({email: e.target.value})
+	}
+
+	changePassword = (e) => {
+		this.setState({password: e.target.value})
+	}
 
 	// Render
 	render() {
@@ -14,16 +31,19 @@ class Signup extends Component {
 			<form>
 				<div className="form-group">
 					<label for="exampleInputEmail1">Nickname</label>
-					<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter nickname..." />
+					<input type="text" value={this.state.name} onChange={(e) => this.changeName(e)}
+					className="form-control" id="exampleInputEmail1" placeholder="Enter nickname..." />
 				</div>
 				<div className="form-group">
 					<label for="exampleInputEmail1">Email address</label>
-					<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+					<input type="email" value={this.state.email} onChange={(e) => this.changeEmail(e)}
+					className="form-control" id="exampleInputEmail1" placeholder="Enter email" />
 					<small id="emailHelp" className="form-text text-muted">Enter a valid Email address</small>
 				</div>
 				<div className="form-group">
 					<label for="exampleInputPassword1">Password</label>
-					<input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+					<input type="password" value={this.state.password} onChange={(e) => this.changePassword(e)}
+					className="form-control" id="exampleInputPassword1" placeholder="Password" />
 				</div>
 				<div className="form-group form-check">
 					<input type="checkbox" className="form-check-input" id="exampleCheck1" />
